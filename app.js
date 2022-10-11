56fc9d0b30c04139886f553ac5a66f93ee6474b9
Zaid <zaidansari42@gmail.com>
2022-10-11 18:07:52 +0530

    Add the JS File

---
+++ app.js
@@ -0,0 +1,11 @@
+var sum = 10 + 5;
+
+console.log(sum);
+
+var myString = "Zaid";
+
+var newMyString = 'Zaid isn\'t "home"';
+console.log(newMyString);
+
+var myStr = "Firstline\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
